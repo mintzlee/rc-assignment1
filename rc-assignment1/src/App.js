15e3d02683739d5cd4b45588f042d880ff5c09b9
Lee Mintz <lee.mintz@bestbuy.com>
2019-12-26 17:47:33 -0600

    Made more nicer-er

--- rc-assignment1/src/App.js
+++ rc-assignment1/src/App.js
@@ -6,26 +6,19 @@ import './App.css';
 const App = props => {
   const [ userNameState, setUserNameState ] = useState(
     {
-      user: "unassigned"
+      userName: "unassigned"
     }
   )
 
   const changeUserNameHandler = ( event ) => {
-    setUserNameState({user: event.target.value})
-  }
-
-  const style= {
-    backgroundColor: "#DDD",
-    font: "inherit",
-    border: "1px solid red",
-    padding: "5px"
+    setUserNameState({userName: event.target.value})
   }
 
   return (
-    <div className="app">
-      <UserInput style={style} onChange={changeUserNameHandler} value={userNameState.user} />
-      <UserOutput userName={userNameState.user} />
-      <UserOutput userName={userNameState.user} />
+    <div className="App">
+      <UserInput onChange={changeUserNameHandler} value={userNameState.userName} />
+      <UserOutput userName={userNameState.userName} />
+      <UserOutput userName={userNameState.userName} />
 
     </div>
 
